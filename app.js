ea3f12e57f57e4f0c138a3ebad016af6f13ae400
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-10-22 23:01:42 +0200

    ready api posts

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
+const bodyParser = require('body-parser');
+const postsRoute=require('./routes/posts');
 
+
+
+app.use(bodyParser.json());
+app.use('/posts', postsRoute);
 
 //Routes
+app.get('/', (req, res)=>{
 
-app.get('/', (req, res) => {
     res.send('We are in home')
-
 })
-app.get('/posts', (req, res) => {
-    res.send('We are in home')
-
-})
-
 
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, () => {
---
+++ routes/posts.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const Post = require('../models/Posts')
+const router = express.Router();
+
+
+
+router.get('/', async (req, res) => {
+    try {
+
+        const posts = await Post.find();
+        res.json(posts)
+    } catch (error) {
+
+    }
+
+})
+
+router.post('/', async (req, res) => {
+
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description,
+    });
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (error) {
+        res.json({ message: error })
+    }
+
+})
+
+
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post)
+    } catch (error) {
+        res.json({ message: error })
+    }
+
+
+})
+
+
+router.delete('/:postId', async (req, res) => {
+    try {
+        const deletepost = await Post.remove({ _id: req.params.postId })
+        res.json(deletepost)
+
+    } catch (error) {
+        res.json({ message: error })
+
+    }
+})
+
+router.patch('/:postId', async (req, res) => {
+
+    try {
+        const updated = await Post.updateOne({ _id: req.params.postId }, 
+            { $set: { title: req.body.title } })
+
+        res.json(updated);
+    } catch (error) {
+        res.json({ message: error })
+
+    }
+
+})
+
+module.exports = router;
